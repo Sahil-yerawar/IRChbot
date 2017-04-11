047ac8d807ae4354f9f72b0cac55398d5f43eae4
Sahil-yerawar <sahil.yerawar@gmail.com>
2017-04-12 01:24:19 +0530

    Refactoring code to include monads in it

--- Project.hs
+++ Project.hs
@@ -3,6 +3,10 @@ import System.IO
 import Text.Printf
 import Data.List
 import System.Exit
+import Control.Arrow
+import Control.Monad.Reader
+import Control.Exception
+
 
 
 ircserver = "irc.freenode.org"
@@ -10,27 +14,53 @@ port   = 6667
 channel   = "#tutbot-testing"
 nick   = "BhadwiAunty"
 
-main = do
-    h <- connectTo ircserver (PortNumber (fromIntegral port))
-    hSetBuffering h NoBuffering
-    write h "NICK" nick
-    write h "USER" (nick++" 0 * :tutorial bot")
-    write h "JOIN" channel
-    listen h
+type Net = ReaderT Bot IO
+data Bot = Bot { socket :: Handle }
 
+main :: IO ()
+main = bracket connect disconnect loop
+    where
+      disconnect = hClose.socket
+      loop st = runReaderT run st
+-- main = do
+--     h <- connectTo ircserver (PortNumber (fromIntegral port))
+--     hSetBuffering h NoBuffering
+--     write h "NICK" nick
+--     write h "USER" (nick++" 0 * :tutorial bot")
+--     write h "JOIN" channel
+--     listen h
+
+connect :: IO Bot
+connect = notify $ do
+  h <- connectTo ircserver (PortNumber (fromIntegral port))
+  hSetBuffering h NoBuffering
+  return (Bot h)
+  where
+    notify a = bracket_
+                (printf "Connecting to %s....." ircserver >> hFlush stdout)
+                (putStrLn "Done.")
+                a
 -- slice' :: Int -> Int -> List -> List
 slice' start end xs = take (end -start) $ drop start xs
-write :: Handle -> String -> String -> IO ()
-write h s t = do
-    hPrintf h "%s %s\r\n" s t
-    printf    "> %s %s\n" s t
+run::Net ()
+run = do
+      write  "NICK" nick
+      write  "USER" (nick++" 0 * :tutorial bot")
+      write  "JOIN" channel
+      asks socket >>= listen
 
-listen :: Handle -> IO ()
+write :: String -> String -> Net ()
+write  s t = do
+    h <- asks socket
+    io $ hPrintf h "%s %s\r\n" s t
+    io $ printf    "> %s %s\n" s t
+
+
+listen :: Handle -> Net ()
 listen h = forever $ do
-    s <- hGetLine h
-    let t = init s
-    if ping t then pong t else evl h (clean t) (sender t) (receiver t)
-    putStrLn s
+    s <- init `fmap` io(hGetLine h)
+    if ping s then pong s else evl (clean s) (sender s) (receiver s)
+    io(putStrLn s)
     -- putStrLn $ receiver t
   where
     forever a = do a; forever a
@@ -39,16 +69,20 @@ listen h = forever $ do
     receiver = takeWhile(/= '\SP').dropWhile(/= '#')
 
     ping x = "PING :" `isPrefixOf` x
-    pong x = write h "PONG" (':' : drop 6 x)
+    pong x = write "PONG" (':' : drop 6 x)
 
-evl :: Handle ->String ->String ->String ->IO ()
-evl h "!quit" y z= write h "QUIT" ":Exiting" >> exitWith ExitSuccess
-evl h x y z
-  | "!id " `isPrefixOf` x  = if take 1 z == ['#'] then msg h (drop 4 x)  else msgPrivate h (drop 4 x) y
-evl _ _ _ _ = return()
+evl :: String ->String ->String ->Net ()
+evl "!quit" y z= write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+evl x y z
+  | "!id " `isPrefixOf` x  = if take 1 z == ['#'] then msg (drop 4 x)  else msgPrivate (drop 4 x) y
+evl _ _ _ = return()
 
-msg :: Handle->String->IO ()
-msg buffer str = write buffer "PRIVMSG" (channel ++ " :" ++ str)
+msg :: String->Net ()
+msg str = write "PRIVMSG" (channel ++ " :" ++ str)
 
-msgPrivate :: Handle->String->String->IO ()
-msgPrivate buffer str sender = write buffer "PRIVMSG" (sender ++ " :" ++ str)
+msgPrivate :: String->String->Net ()
+msgPrivate str sender = write "PRIVMSG" (sender ++ " :" ++ str)
+
+--a function to convert the net monad back into the IO type
+io:: IO a -> Net a
+io = liftIO
